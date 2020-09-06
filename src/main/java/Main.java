7a88420f65abf7557307c5d4a81043319ba15fc3
delshay <damian.antoniak@hotmail.com>
2020-09-06 21:56:04 +0200

    Main class refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,3 @@
-import mediatype.Playable;
-import mediatype.Movie;
-import mediatype.Playlist;
-import mediatype.Song;
-
-import java.util.Comparator;
-
-import static playmode.PlayMode.*;
-
 public class Main {
 
     public static void main(String[] args) {
